38883f6d9f15ae03eb507596de1463999288fad0
Sabin <sabin@sabin.home>
2017-04-15 12:39:47 -0400

    Added logical clocks file

---
+++ src/logicalclocks.cpp
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+
+/*
+	Global Constants
+*/
+#define 
+
+/*
+	Global Variables.
+*/
+int clock;
+
+/*
+	Main method to execute the program logic.
+*/
+int main(int argc, char **argv)
+{
+	
+}
